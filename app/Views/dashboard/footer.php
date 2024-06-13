38fbc03b559057847f73092580b7558383e40255
areeyeo <areeyeooo@gmail.com>
2024-06-13 11:48:15 +0700

    first commit

---
+++ app/Views/dashboard/footer.php
@@ -0,0 +1,7 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Pet Journey Agency</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.0
+    </div>
+</footer>
